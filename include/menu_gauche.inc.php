f57d6457bf1086c106721fd8a2912bf0675a13dd
Emmanuelle <emmanuelle.thiroloix@gmail.com>
2019-04-08 11:15:02 +0200

    ajout page défis

--- include/menu_gauche.inc.php
+++ include/menu_gauche.inc.php
@@ -14,7 +14,7 @@
                 </a>
             </li>
             <li class="sub-menu">
-                <a href="liste_connexion.php" <?php if($page == "liste_connexion.php" || $page=="ajouter_qq.php"){echo 'class="active"';} ?>>
+                <a href="liste_connexion.php" <?php if($page == "liste_connexion.php"){echo 'class="active"';} ?>>
                     <i class="fa fa-info-circle"></i>
                     <span>Mon réseau</span>
                 </a>
@@ -26,6 +26,18 @@
                 </a>
                 <ul class="sub">
                     <li <?php if($page == "modification_information.php" ){echo 'class="active"';} ?>><a href="modification_information.php">Informations personnelles</a></li>
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;" <?php if($page == "defis_en_cours.php" || $page=="defis_en_attente.php"){echo 'class="active"';} ?>>
+                    <i class="fa fa-signal"></i>
+                    <span>Défis</span>
+                </a>
+                <ul class="sub">
+                    <li <?php if($page == "defis_en_cours.php" ){echo 'class="active"';} ?>><a href="defis_en_cours".php">Mes défis en cours</a></li>
+                </ul>
+                <ul class="sub">
+                    <li <?php if($page == "defis_en_attente.php" ){echo 'class="active"';} ?>><a href="defis_en_attente.php">Mes défis en attente</a></li>
                 </ul>
             </li>
         </ul></div>        
