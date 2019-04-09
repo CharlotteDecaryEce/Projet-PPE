33f5239b1b2c6b034bb216cdb169bfe1c3b2ea76
Baptiste Grobon <baptiste.grobon@gmail.com>
2019-04-09 14:44:25 +0200

    barre nav

--- include/menu_haut.inc.php
+++ include/menu_haut.inc.php
@@ -6,7 +6,23 @@ $req=$pdo->prepare('SELECT * FROM informations WHERE id = ?');
       $moi=$req->fetch();
 $like_a_distrib=$moi->likes_distrib;
 $likes_recus=$moi->likes_recus;
-
+    $req=$pdo->prepare('SELECT * FROM informations WHERE id = ?');
+    $req->execute([$_SESSION['auth']->id]);
+    $moi=$req->fetch();
+    $nb_like_distrib=$moi->likes_distrib;
+    $nb_like_recus=$moi->likes_recus;
+    if($nb_like_recus==''){
+        $nb_like_recus='0';
+    }
+    if($nb_like_distrib==''){
+        $nb_like_distrib='0';
+    }
+    $competences=$moi->competences_acquises;
+    if($competences!=''){
+        $array_comp=explode(',', $competences);
+        $nb_comp=count($array_comp);
+    }
+    else $nb_comp='0';
 ?>
 
 <section id="container" >
@@ -24,15 +40,20 @@ $likes_recus=$moi->likes_recus;
     </div>
 </div>
 <!--logo end-->
-
-<div class="nav notify-row" id="top_menu">
-    <!--  notification start -->
-    <!-- settings end -->
-</div>
 <div class="top-nav clearfix">
-    <!--search & user info start-->
+ <ul class="nav pull-left top-menu1">
+<h4><?php echo $nb_comp ?></h4>
+<h4>Softskills</h4>
+</ul>
+<ul class="nav pull-left top-menu2">
+<h4><?php echo $nb_like_recus ?></h4>
+<h4>Likes reçus </h4>
+</ul>
+<ul class="nav pull-left top-menu3">
+<h4><?php echo $nb_like_distrib ?></h4>
+<h4>Likes à distribuer</h4>
+</ul>
     <ul class="nav pull-right top-menu">
-             <li class="fa fa-thumbs-up"><span class="badge">42</span></li>
         <!-- user login dropdown start-->
         <li class="dropdown">
             <a data-toggle="dropdown" class="dropdown-toggle" href="modification_information.php">
