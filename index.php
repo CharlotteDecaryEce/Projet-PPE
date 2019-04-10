b5f675639d61dcec9d9d1e3760585cb9922956b7
Baptiste Grobon <baptiste.grobon@gmail.com>
2019-04-10 14:40:16 +0200

    conn terminée

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ if(isset($_SESSION['auth'])){
 }
 if(empty($_POST)){
  if(empty($_POST['username'])){
-  $_SESSION['flash']['danger'] = "Vous n'avez pas saisie de pseudo!";
-  $error="Veuillez saisir un pseudo ";
+  $_SESSION['flash']['danger'] = "Vous n'avez pas saisie d'identifiant!";
+  $error="Veuillez saisir un identifiant ";
 }
 else if(empty($_POST['password'])){
   $_SESSION['flash']['danger'] = "Vous n'avez pas saisie de mot de passe!";
@@ -58,7 +58,7 @@ include("include/header.inc.php");
     <div class="container">
 
       <form class="form-signin" action="" method="POST">
-        <h2 class="form-signin-heading">Connecte toi !</h2>
+        <center><div class="form-signin-heading"><img src="images/logocon.png" alt="" /></div></center>
         <div class="login-wrap">
           
             <div class="user-login-info">
@@ -75,21 +75,15 @@ include("include/header.inc.php");
                 <input type="text" class="form-control" name="username" placeholder="Pseudo" autofocus>
                 <input type="password" class="form-control" name="password" placeholder="Mot de passe">
             </div>
-            <label class="checkbox">
-                <input type="checkbox" name="remember" value="remember"> Mémoriser
-                <span class="pull-right">
-                    <a data-toggle="modal" href="#myModal"> Mot de passe oublié?</a>
-
-                </span>
             </label>
-            <button class="btn btn-lg btn-login btn-block" type="submit"> Connexion </button>
+            <button class="btn btn-lg btn-login btn-block" type="submit"> Brise la Glace ! </button>
 
-            <div class="registration">
-                Pas encore de compte?
+         <center>   <div class="registration">
                 <a class="" href="inscription.php">
                     Créer mon compte
                 </a>
             </div>
+        </center>
 
         </div>
 
