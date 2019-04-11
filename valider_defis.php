2a139a363480ed3ba4208af6e3c83277151b3c28
Emmanuelle <emmanuelle.thiroloix@gmail.com>
2019-04-11 10:55:42 +0200

    competence_voulu & acquise enlevees

--- valider_defis.php
+++ valider_defis.php
@@ -11,40 +11,59 @@ $req=$pdo->prepare('SELECT * FROM informations WHERE id = ?');
       $req->execute([$_SESSION['auth']->id]);
       $moi=$req->fetch();
 
-$competenc_acquises='';
-
+$competences_acquises='';
+$competences_voulues='';
+$pas_ok=0;
 if($moi->competences_acquises!=''){
-      $competence_acquise=$moi->competences_acquises.",".$defis->competences_acquises;
+      $competences_acquises=$moi->competences_acquises;
+      foreach (explode(',',$competences_acquises) as $c) {
+            if($c==$defis->competences_acquises){
+                  $pas_ok=1;
+            }
+      }
+      if($pas_ok==0){
+            if($competences_acquises!=''){
+                  $competences_acquises=$competences_acquises.','.$defis->competences_acquises;
+            }
+            else $competences_acquises=$defis->competences_acquises;
+      }
 }
-else $competence_acquise=$defis->competences_acquises;
+else $competences_acquises=$defis->competences_acquises;
 
-if($moi->competences_acquises!=''){
-      $competenc_acquises=explode(',',$moi->competences_acquises);
+if($moi->competences!='' && $moi->competences_acquises!=''){
+      $comp_enl='';
+      foreach (explode(',',$moi->competences_acquises) as $c_acq) {
+            if($c_acq==$defis->competences_acquises){
+                  foreach (explode(',',$moi->competences) as $ma_compet) {
+                        if($ma_compet==$c_acq){
+                              $comp_enl=$c_acq;
+                        }
+                  }
+            }
+      }
+      if($comp_enl!=''){
+            foreach (explode(',',$moi->competences) as $c) {
+                  if($c != $comp_enl){
+                        if($competences_voulues!=''){
+                              $competences_voulues=$competences_voulues.','.$c;
+                        }
+                        else{
+                              $competences_voulues=$c;
+                        }
+                  }
+            }
+      }
 }
-else $competenc_acquises='';
+else $competences_voulues=$moi->competences;
 
 
-$compe='';
-$ok=1;
 $req=$pdo->prepare('SELECT competences FROM informations WHERE id = ?');
                   $req->execute([$_SESSION['auth']->id]);
                   $comp_voulu=$req->fetch();
 
-if($competenc_acquises!='')
-{
-      foreach ($competenc_acquises as $c) {
-            if($c=!$defis->competences) {
-                  if($compe!=''){
-                        $compe=$compe.','.$c;
-                  }
-                  else $compe=$c;
-            }
-      }
-}
-//ajout compétence acquise
-if($ok==1){
-      $req=$pdo->prepare('UPDATE informations SET competences_acquises=?, competences=? WHERE id = ?')->execute([$competence_acquise,$compe, $_SESSION['auth']->id]);
-}
+
+      $req=$pdo->prepare('UPDATE informations SET competences_acquises=?, competences=? WHERE id = ?')->execute([$competences_acquises,$competences_voulues, $_SESSION['auth']->id]);
+
 
 $req=$pdo->prepare('SELECT * FROM informations WHERE id = ?');
       $req->execute([$_SESSION['auth']->id]);
